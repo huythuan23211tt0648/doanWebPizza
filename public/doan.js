c676375c11661653775981ac3b611e38991a94a2
huythuan23211tt0648 <huythuan7a4@gmail.com>
2024-06-11 15:56:30 +0700

    first commit

---
+++ public/doan.js
@@ -0,0 +1,27 @@
+// css them
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("hahaha");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+
+  });
+}
+// css xoa active
+const links = document.querySelectorAll('.hahaha');
+    
+if (links.length) {
+  links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      links.forEach((link) => {
+          link.classList.remove('active');
+      });
+      e.preventDefault();
+      link.classList.add('active');
+    });
+  });
+}
